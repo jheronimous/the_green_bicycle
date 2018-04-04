c9b11601cb5509c1bf2b04d6bb0354deab869f3e
Joe Robb <joerobb@joes-air.sparkpeople.com>
2018-04-04 16:57:59 -0400

    change styles

--- app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
+++ app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
@@ -2,6 +2,11 @@ package com.example.android.thegreenbicycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Absurd extends AppCompatActivity {
 

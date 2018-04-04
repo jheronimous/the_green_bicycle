4576959959c861d00e8c1c25c482535aa9c7181d
Joe Robb <joerobb@joes-air.sparkpeople.com>
2018-04-04 12:41:41 -0400

    initial commit

---
+++ app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
@@ -0,0 +1,13 @@
+package com.example.android.thegreenbicycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Absurd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absurd);
+    }
+}

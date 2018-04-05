b0afe07de6f8b9db22fc8dbcc1db72043ee9fdc8
Joe Robb <joerobb@joes-air.sparkpeople.com>
2018-04-05 13:02:39 -0400

    change layout

--- app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
+++ app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
@@ -7,12 +7,22 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+/**
+ * This app displays a final score, based on completed, correct answers.
+ */
 
 public class Absurd extends AppCompatActivity {
-
+    int score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absurd);
     }
+
 }

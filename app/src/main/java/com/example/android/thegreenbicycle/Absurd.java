42db6db6acae4dcec77e319304111a98cf8dda3a
Joe Robb <joerobb@Joes-MacBook-Air.local>
2018-04-11 00:05:27 -0400

    FINAL

--- app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
+++ app/src/main/java/com/example/android/thegreenbicycle/Absurd.java
@@ -15,7 +15,22 @@ import android.widget.Toast;
  */
 
 public class Absurd extends AppCompatActivity {
-/**
+
+    private RadioButton Question1RadioButton;
+    private RadioButton Question2SecondRadioButton;
+    private RadioButton radioButtonQuestion5;
+    private RadioButton radioButtonQuestion6;
+    private EditText editTextQuestion3;
+    private CheckBox Question4FirstCheckBox;
+    private CheckBox Question4SecondCheckBox;
+    private CheckBox Question4ThirdCheckBox;
+    private CheckBox Question4FourthCheckBox;
+    private RadioGroup question6RadioGroup;
+    private RadioGroup question5RadioGroup;
+    private RadioGroup question2RadioGroup;
+    private RadioGroup question1RadioGroup;
+
+    /**
     Setting the initial value of the score total to 0.
  */
     int scoreTotal = 0;
@@ -24,6 +39,19 @@ public class Absurd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absurd);
+        Question1RadioButton = findViewById(R.id.questionOneFalse);
+        Question2SecondRadioButton = findViewById(R.id.questionTwoRhinoceroses);
+        editTextQuestion3 = findViewById(R.id.questionThreeAnswer);
+        radioButtonQuestion5 = findViewById(R.id.questionFiveAnswerThree);
+        radioButtonQuestion6 = findViewById(R.id.questionSixTrue);
+        Question4FirstCheckBox = findViewById(R.id.questionFourComfortablePants);
+        Question4SecondCheckBox = findViewById(R.id.questionFourUncomfortablePants);
+        Question4ThirdCheckBox = findViewById(R.id.questionFourCockroach);
+        Question4FourthCheckBox = findViewById(R.id.questionFourUngeziefer);
+        question6RadioGroup = findViewById(R.id.radioUbu);
+        question5RadioGroup = findViewById(R.id.radioVonnegut);
+        question2RadioGroup = findViewById(R.id.radioIonesco);
+        question1RadioGroup = findViewById(R.id.radioGogol);
     }
 
 /**
@@ -32,7 +60,6 @@ public class Absurd extends AppCompatActivity {
     public void submitButton1(View view) {
 
         //Checking the correct answer is marked for question #1. If so, add 1 to score total.
-        RadioButton Question1RadioButton = findViewById(R.id.question1_false);
         boolean question1Answer = Question1RadioButton.isChecked();
 
         if (question1Answer) {
@@ -42,8 +69,7 @@ public class Absurd extends AppCompatActivity {
     }
     public void submitButton2(View view){
         //Checking the correct answer is marked for question #2. If so, add 1 to score total.
-        RadioButton question2SecondRadioButton = findViewById(R.id.question2_rhinoceroses);
-        boolean question2Answer = question2SecondRadioButton.isChecked();
+        boolean question2Answer = Question2SecondRadioButton.isChecked();
 
         if (question2Answer) {
             scoreTotal = scoreTotal + 1;
@@ -51,8 +77,6 @@ public class Absurd extends AppCompatActivity {
 
     }
     public void submitButton3(View view){
-
-        EditText editTextQuestion3 = findViewById(R.id.question3_answer);
         String theLetterE = editTextQuestion3.getText().toString();
         if (theLetterE.equalsIgnoreCase(getString(R.string.theLetterE))) {
             scoreTotal = scoreTotal + 1;
@@ -61,7 +85,6 @@ public class Absurd extends AppCompatActivity {
     }
     public void submitButton5(View view){
         //Checking the correct answer is marked for question #5. If so, add 1 to score total.
-        RadioButton radioButtonQuestion5 = findViewById(R.id.question5_answer3);
         boolean question5Answer = radioButtonQuestion5.isChecked();
 
         if (question5Answer) {
@@ -71,7 +94,6 @@ public class Absurd extends AppCompatActivity {
     }
     public void submitButton6(View view){
         //Checking the correct answer is marked for question #6. If so, add 1 to score total.
-        RadioButton radioButtonQuestion6 = findViewById(R.id.question6_true);
         boolean question6Answer = radioButtonQuestion6.isChecked();
 
         if (question6Answer) {
@@ -81,16 +103,12 @@ public class Absurd extends AppCompatActivity {
     }
     public void submitButton4(View view){
         //Checking the correct answer is marked for question #4. If so, add 1 to score total.
-        CheckBox Question4FirstCheckBox = findViewById(R.id.question4_comfortable_pants);
         boolean question4_first_checkbox = Question4FirstCheckBox.isChecked();
 
-        CheckBox Question4SecondCheckBox = findViewById(R.id.question4_uncomfortable_pants);
         boolean question4_second_checkbox = Question4SecondCheckBox.isChecked();
 
-        CheckBox Question4ThirdCheckBox = findViewById(R.id.question4_cockroach);
         boolean question4_third_checkbox = Question4ThirdCheckBox.isChecked();
 
-        CheckBox Question4FourthCheckBox = findViewById(R.id.question4_ungeziefer);
         boolean question4_fourth_checkbox = Question4FourthCheckBox.isChecked();
 
         if (question4_third_checkbox && question4_fourth_checkbox) {
@@ -114,38 +132,34 @@ public class Absurd extends AppCompatActivity {
         toast.show();
 
         //Resetting the selections in the quiz when the Reset button is clicked
-        RadioGroup question1RadioGroup = findViewById(R.id.radioGogol);
         question1RadioGroup.clearCheck();
 
-        RadioGroup question2RadioGroup = findViewById(R.id.radioIonesco);
         question2RadioGroup.clearCheck();
 
-        EditText question3EditText = findViewById(R.id.question3_answer);
+        EditText question3EditText = findViewById(R.id.questionThreeAnswer);
         question3EditText.getText().clear();
 
-        CheckBox question4FirstCheckBox = findViewById(R.id.question4_comfortable_pants);
+        CheckBox question4FirstCheckBox = findViewById(R.id.questionFourComfortablePants);
         if (question4FirstCheckBox.isChecked()) {
             question4FirstCheckBox.setChecked(false);
         }
-        CheckBox question4SecondCheckBox = findViewById(R.id.question4_uncomfortable_pants);
+        CheckBox question4SecondCheckBox = findViewById(R.id.questionFourUncomfortablePants);
         if (question4SecondCheckBox.isChecked()) {
             question4SecondCheckBox.setChecked(false);
         }
 
-        CheckBox question4ThirdCheckBox = findViewById(R.id.question4_cockroach);
+        CheckBox question4ThirdCheckBox = findViewById(R.id.questionFourCockroach);
         if (question4ThirdCheckBox.isChecked()) {
             question4ThirdCheckBox.setChecked(false);
         }
 
-        CheckBox question4FourthCheckBox = findViewById(R.id.question4_ungeziefer);
+        CheckBox question4FourthCheckBox = findViewById(R.id.questionFourUngeziefer);
         if (question4FourthCheckBox.isChecked()) {
             question4FourthCheckBox.setChecked(false);
         }
 
-        RadioGroup question5RadioGroup = findViewById(R.id.radioVonnegut);
         question5RadioGroup.clearCheck();
 
-        RadioGroup question6RadioGroup = findViewById(R.id.radioUbu);
         question6RadioGroup.clearCheck();
 
         scoreTotal = 0;
